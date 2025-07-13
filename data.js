0743c98660546c1da3a7967a6c4021f01669d177
Akihisa Shimizu <akihisa2924@gmail.com>
2025-07-14 02:39:21 +0900

    Initial commit

---
+++ data.js
@@ -0,0 +1,269 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "ダイニング",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6245431612930616,
+          "pitch": 0.052756473950609006,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -1.3035700183277665,
+          "pitch": 0.043313761796657246,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 1.2129099487616894,
+          "pitch": 0.015708709157701506,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 2.0425313964740255,
+          "pitch": 0.10183318538377151,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -2.305675246714145,
+          "pitch": 0.11772976906063626,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "キッチン",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.43451825999498617,
+          "pitch": 0.15583567932123188,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "リビング",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.742775999136965,
+          "pitch": 0.32546978947411986,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -2.72387428425046,
+          "pitch": 0.021898162057922832,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -2.4490096725819743,
+          "pitch": 0.1042644553208234,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -3.1399619724992203,
+          "pitch": 0.02481374334438513,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.9088648708512768,
+          "pitch": 0.011042460647143315,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-",
+      "name": "庭",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.65388704447418,
+          "pitch": -0.015630179339822803,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "シアタールーム",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9035141727924376,
+          "pitch": 0.048533697222822525,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "書斎",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0392079979994833,
+          "pitch": 0.06955100259038716,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
